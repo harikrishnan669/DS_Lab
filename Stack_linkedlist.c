0a49ff809e05a95e319d35da34df05a652deebe3
HARIKRISHNAN R <128880349+kte22cs061@users.noreply.github.com>
2024-02-11 13:27:52 +0530

    Create Stack_linkedlist.c

---
+++ Stack_linkedlist.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node1
+{
+    int data;
+    struct node1 *link;
+}node;
+node *top=NULL;
+void push()
+{
+    int num;
+    node *new;
+    printf("Enter data");
+    scanf("%d",&num);
+    new=(node*)malloc(sizeof(node));
+    new->data=num;
+    new->link=top;
+    top=new;
+}
+void pop()
+{
+    if(top==NULL)
+    {
+        printf("Stack empty");
+        return;
+    }
+    node *temp=top;
+    top=top->link;
+    free(temp);
+}
+void display()
+{
+    if(top==NULL)
+    {
+        printf("Stack empty");
+        return;
+    }
+    node *temp=top;
+    while(temp!=NULL)
+    {
+        printf("%d\n",temp->data);
+        temp=temp->link;
+    }
+}
+int main()
+{
+    int choice;
+    do
+    {
+        printf("Enter your choice\n");
+        printf("1-push\n");
+        printf("2-pop\n");
+        printf("3-Display\n");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:push();
+            break;
+            case 2:pop();
+            break;
+            case 3:display();
+            break;
+        }
+    }while(choice<=3);
+}
